f70632c7f7aa9ded9ef472ab120f0d76b2b9d032
Dorothea Toeller <toellerdorothea@gmail.com>
2024-09-30 11:46:16 +0100

    add validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :bookmarks
   has_many :categories, through: :bookmarks
-  validates :name, presence: true
-  validates :rating,
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0.0, less_than_or_equal_to: 10.0, message: "not in valid range!"}
 end
